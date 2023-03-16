6430691eb65deee459ec2cb44ee5affcd3a7e430
Luca Dei Zotti <luca.deizotti@gmail.com>
2023-03-17 00:23:43 +0100

    [add] basic cookie/node and result bindings

--- lib/cluster_connect_smartcell.ex
+++ lib/cluster_connect_smartcell.ex
@@ -11,20 +11,31 @@ defmodule ClusterConnectSmartcell do
   def init(attrs, ctx) do
     root_fields = %{
       "target_node" => attrs["target_node"] || "",
+      "target_node_bound" => attrs["target_node_bound"] || "",
       "erlang_cookie" => attrs["erlang_cookie"] || "",
+      "erlang_cookie_bound" => attrs["erlang_cookie_bound"] || "",
       "module" => attrs["module"] || "",
       "function" => attrs["function"] || "",
-      "arguments" => attrs["arguments"] || ""
+      "arguments" => attrs["arguments"] || "",
+      "bind_result" => Kino.SmartCell.prefixed_var_name("rpc_result", attrs["bind_result"])
     }
 
-    {:ok, assign(ctx, root_fields: root_fields)}
+    ctx =
+      ctx
+      |> assign(root_fields: root_fields)
+      |> assign(node_bounds: %{})
+      |> assign(cookie_bounds: %{})
+
+    {:ok, ctx}
   end
 
   @impl true
   def handle_connect(ctx) do
     # Called first time the client connects, gives initial state
     payload = %{
-      root_fields: ctx.assigns.root_fields
+      root_fields: ctx.assigns.root_fields,
+      node_bounds: ctx.assigns.node_bounds,
+      cookie_bounds: ctx.assigns.cookie_bounds
     }
 
     {:ok, payload, ctx}
@@ -39,40 +50,44 @@ defmodule ClusterConnectSmartcell do
   def to_source(attrs) do
     %{
       "target_node" => target_node,
+      "target_node_bound" => target_node_bound,
       "erlang_cookie" => erlang_cookie,
+      "erlang_cookie_bound" => erlang_cookie_bound,
       "module" => module,
       "function" => function,
-      "arguments" => arguments
+      "arguments" => arguments,
+      "bind_result" => bind_result
     } = attrs
 
-    target_node = String.to_atom(target_node)
-    erlang_cookie = String.to_atom(erlang_cookie)
+    target_node = target_node |> get_target_node(target_node_bound) |> String.to_atom()
+    erlang_cookie = erlang_cookie |> get_erlang_cookie(erlang_cookie_bound) |> String.to_atom()
 
     quote do
-      with {:cookie, true} <-
-             {:cookie, :erlang.set_cookie(unquote(erlang_cookie))},
-           {:connect, true} <-
-             {:connect,
-              unquote(target_node)
-              |> :net_kernel.connect_node()},
-           {:monitor, :ok} <- {:monitor, :net_kernel.monitor_nodes(true)},
-           nodes <- Node.list(:connected),
-           {:command, {:ok, result}} <-
-             {:command,
-              ClusterConnectSmartcell.do_execute_remote_command(
-                unquote(target_node),
-                unquote(module),
-                unquote(function),
-                unquote(arguments)
-              )} do
-        %{"result" => result, "nodes" => nodes}
-      else
-        {step, res} when step in [:cookie, :connect, :command, :monitor] ->
-          %{"outcome" => false} |> Map.put(Atom.to_string(step), res)
+      unquote(quoted_var(bind_result)) =
+        with {:cookie, true} <-
+               {:cookie, :erlang.set_cookie(unquote(erlang_cookie))},
+             {:connect, true} <-
+               {:connect,
+                unquote(target_node)
+                |> :net_kernel.connect_node()},
+             {:monitor, :ok} <- {:monitor, :net_kernel.monitor_nodes(true)},
+             nodes <- Node.list(:connected),
+             {:command, {:ok, result}} <-
+               {:command,
+                ClusterConnectSmartcell.do_execute_remote_command(
+                  unquote(target_node),
+                  unquote(module),
+                  unquote(function),
+                  unquote(arguments)
+                )} do
+          %{"result" => result, "nodes" => nodes}
+        else
+          {step, res} when step in [:cookie, :connect, :command, :monitor] ->
+            %{"outcome" => false} |> Map.put(Atom.to_string(step), res)
 
-        error ->
-          %{"outcome" => false, "error" => error}
-      end
+          error ->
+            %{"outcome" => false, "error" => error}
+        end
     end
     |> Kino.SmartCell.quoted_to_string()
   end
@@ -93,7 +108,7 @@ defmodule ClusterConnectSmartcell do
   def handle_info({:scan_binding_result, data}, ctx) do
     ctx = assign(ctx, data_options: data)
 
-    broadcast_event(ctx, "update_binding", data)
+    broadcast_event(ctx, "update_binding", %{"bindings" => data})
 
     {:noreply, ctx}
   end
@@ -102,6 +117,7 @@ defmodule ClusterConnectSmartcell do
   Try to identify possible autocompletion targets
 
   possible nodes: atoms contianing "@" character
+  possible secrets: atoms
   """
   @impl true
   def scan_binding(pid, binding, _env) do
@@ -109,13 +125,23 @@ defmodule ClusterConnectSmartcell do
       binding
       |> Enum.filter(fn {_key, value} -> is_atom(value) end)
       |> Enum.filter(fn {_key, value} -> value |> Atom.to_string() |> String.contains?("@") end)
+      |> Enum.into(%{})
+
+    possible_secrets =
+      binding
+      |> Enum.filter(fn {_key, value} -> is_atom(value) end)
+      |> Enum.into(%{})
 
     data = %{
-      possible_nodes: possible_nodes
+      possible_nodes: possible_nodes,
+      possible_secrets: possible_secrets
     }
 
     send(pid, {:scan_binding_result, data})
   end
+
+  # defp to_select_pair({key, value}) when is_atom(key), do: {Atom.to_string(key), value}
+  # defp to_select_pair({key, value}), do: {key, value}
 
   # NOTE: this function needs to remain public, so it can be called by generated code
   # Candidate for refactor :)
@@ -138,6 +164,9 @@ defmodule ClusterConnectSmartcell do
     do: {:ok, :no_command}
 
   # Privates
+  defp quoted_var(value) when is_binary(value), do: {String.to_atom(value), [], nil}
+  defp quoted_var(value) when is_atom(value), do: {value, [], nil}
+
   defp parse_value(_field, value), do: value
 
   defp normalize_module_name(module) do
@@ -153,4 +182,12 @@ defmodule ClusterConnectSmartcell do
 
   defp parse_evaluated_arguments!({arguments, _bindings}),
     do: raise("Arguments should be a list, got #{inspect(arguments)}")
+
+  defp get_target_node("", target_node_bound), do: target_node_bound
+  defp get_target_node(target_node, ""), do: target_node
+  defp get_target_node(_target_node, target_node_bound), do: target_node_bound
+
+  defp get_erlang_cookie("", erlang_cookie_bound), do: erlang_cookie_bound
+  defp get_erlang_cookie(erlang_cookie, ""), do: erlang_cookie
+  defp get_erlang_cookie(_target_node, erlang_cookie_bound), do: erlang_cookie_bound
 end
