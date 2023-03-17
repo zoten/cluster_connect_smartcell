2c13f871427a5789b59006ccd85b3912332f1d40
Luca Dei Zotti <luca.deizotti@gmail.com>
2023-03-17 18:12:14 +0100

    [0.0.2] Changelog
     * result variable binding
     * context variables as parameters
     * small css hammering

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ClusterConnectSmartcell.MixProject do
   def project do
     [
       app: :cluster_connect_smartcell,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
